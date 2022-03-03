d8a85d5c9c5cf29ec773ef7810aed1a76ec59aa3
Aylin <gamena750@abv.bg>
2022-03-03 22:47:21 +0000

    Simple tests working
    Need to do few more things

--- UnitTests/FibonacciSequence.test.js
+++ UnitTests/FibonacciSequence.test.js
@@ -6,12 +6,8 @@ test('This number should be positive ', () => {
     expect(randomNum.add()).toBeLessThan(1000);
     expect(randomNum.add()).toBeGreaterThanOrEqual(1);
 });
-test('This number should be positive ', () => {
-
-   
-
-    
-    //expect((randomNum.addNum1())).toBeGreaterThanOrEqual(1);
-    //expect((randomNum.addNumNext())).toBeLessThan(1000);
+test('This number should be positive ', () => {    
+    expect((randomNum.addNum1())).toBeGreaterThanOrEqual(1);
+    expect((randomNum.addNumNext())).toBeLessThan(1000);
     //expect(functions.add()).toBeGreaterThanOrEqual(1);
 });
