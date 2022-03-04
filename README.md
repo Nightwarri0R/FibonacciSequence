eb382977b33b2fe6ea7d28fcebdb7f5b2aac8d94
Aylin <gamena750@abv.bg>
2022-03-04 10:57:43 +0000

    Few quick final touches
    Added instructions on how to run this program

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Requirements
 1. Print out Fibonacci sequence numbers 
 2. Based on a randomly generated number 
 3. Write unit test for it
+
+How to run this program on linux. 
+1. Go to App follder
+2. Open a terminal and enter this "node FibonacciSequence.js".
+How to run tests. 
+1. Go to UnitTests follder
+2. Open a terminal in this folder and run "npm tests"
--- UnitTests/FibonacciSequence.test.js
+++ UnitTests/FibonacciSequence.test.js
@@ -6,7 +6,7 @@ test('This number should be positive ', () => {
     expect(tests.add()).toBeLessThan(31);
     expect(tests.add()).toBeGreaterThanOrEqual(1);
 });
-//Test function thhat tests that array imported from the javascript file is all correct by comparing it to two identicall instances arrays of Fibonacci sequence
+//Test function thhat tests that array imported from the javascript file is all correct by comparing it to two identicall instances  of the array of Fibonacci sequence
 test('This is the Fibonacci Sequence numbers ', () => {    
     const fibTest = tests.sequence(); 
     const arrayTest = tests.sequence();
